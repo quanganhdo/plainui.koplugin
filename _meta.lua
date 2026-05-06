6014bd8f3a81f56029c5b4c092efe933a9b5504e
Anh Do <hello@anh.do>
2026-05-06 18:52:13 -0400

    Version 1.2

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,6 @@ return {
     name = "plainui",
     fullname = "Plain UI",
     description = [[File manager metadata tabs and cover annotations.]],
-    version = "0.1.0",
+    version = "1.2",
     author = "Anh Do",
 }
